1d57c023e1e8b5eb84f1a8fbabcbc3b69378011b
Liev Garcia <liev.garcia@gmail.com>
2018-06-08 13:11:47 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='This is a small project to find the rotation angle of a given image using neural network',
+    author='Liev Garcia',
+    license='MIT',
+)
